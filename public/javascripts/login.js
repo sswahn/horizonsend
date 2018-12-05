afe66fec99c005cc1a9b1ab59bf829b625f28441
steve <swahn@protonmail.com>
2018-12-05 12:11:56 -0500

    login form styling

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,10 +5,12 @@
 
 Vue.component('login-form', {
   template: `
-    <form class="form form-small" v-on:submit.prevent="submitForm">
+    <form id="login-form" v-on:submit.prevent="submitForm">
+    <img class="logo" src="http://www.horizonsend.com/images/horizons-end_logo.png" alt="Horizon's End | Video Game Design">
       <input id="email" type="email" name="email" placeholder="Email" required>
       <input id="password" type="password" name="password" placeholder="Password" required>
-      <button type="submit">Log In</button>
+      <button type="submit">Log in to Horizon's End</button>
+      <a href="/register">Create an account.</a>
     </form>
   `,
   methods: {
@@ -23,9 +25,10 @@ Vue.component('login-form', {
         headers: { 
           'Content-Type': 'application/json'
         }
-      }).then(response => response.json())
-        .then(data => (data.error) ? alert(data.error) : window.location.href = '/admin') // change this
-        .catch(error => console.error(error))
+      })
+      .then(response => response.json())
+      .then(data => (data.error) ? alert(data.error) : window.location.href = '/admin') // change this
+      .catch(error => console.error(error))
     }
   }
 })
--- routes/loginPage.js
+++ routes/loginPage.js
@@ -5,6 +5,7 @@ const router = express.Router()
 router.get('/login', function(request, response) {
   response.render('login', { 
     title: "Horizon's End | Video Game Design",
+    css: 'login.css',
     script: 'login.js'
   })
 })
